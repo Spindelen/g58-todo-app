28acb3110641cee1b03809165e2f3cc2227d23b4
Spindelen <erik@manboy.se>
2026-02-04 02:08:32 +0100

    added basic Crud operations

--- src/main/java/se/lexicon/g58todoapp/controller/TodoController.java
+++ src/main/java/se/lexicon/g58todoapp/controller/TodoController.java
@@ -3,4 +3,43 @@ package se.lexicon.g58todoapp.controller;
 public class TodoController {
 
     //TODO: Implement basic CRUD operations for Todo entity
+
+    //localhost:8080/api/people for all the endpoint in this class.
+
+
+
+        private final TodoRepository todoRepository;
+
+        public TodoController(TodoRepository todoRepository){
+            this.todoRepository= todoRepository;
+
+        }
+
+        @GetMapping("/{id}")
+        public Todo getTodoId(@PathVariable Long id){
+            return todoRepository.findById(id).orElseThrow(RuntimeException::new);
+        }
+        //POST
+        @ResponseStatus(HttpStatus.CREATED)
+        @PostMapping
+        public void createTodo(@RequestBody Todo todo){
+            todoRepository.save(todo);
+        }
+        //DELETE
+        @DeleteMapping("/{id}")
+        @ResponseStatus(HttpStatus.NO_CONTENT)
+        public void deleteTodo(@PathVariable Long id){
+            todoRepository.deleteById(id);
+        }
+
+
+
+        //RequestParam = ?name=Simon
+        //GET localhost:8080/api/hello?name=Simon
+        @RequestMapping(value = "/hello")
+        public String hello (@RequestParam(defaultValue = "from PersonController!") String name) {
+            return "Hello, " + name;
+        }
+
+
 }
